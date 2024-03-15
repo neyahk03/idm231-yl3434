85c70ab3418c39a932995c42aa30b4fd6fc5459c
Yen Luong <yl3434@drexel.edu>
2024-03-15 10:06:27 -0400

    :construction: Updated sound

--- js/app.js
+++ js/app.js
@@ -7,11 +7,7 @@ const caption = document.querySelector('figcaption');
 const box = document.querySelector('.content');
 const sound = document.querySelector('#audio_player');
 const close = document.querySelector('.close');
-// const pauseButton = document.querySelector('.pause');
 
-// pauseButton.addEventListener('click', () => {
-//     sound.pause();
-// })
 
 const helpButton = document.querySelector('.help');
 helpButton.addEventListener('click', () => {
@@ -21,8 +17,8 @@ helpButton.addEventListener('click', () => {
 close.addEventListener ('click', () => {
     console.log('its working');
     box.style.opacity = '0';
+    sound.src = "";
 })
-
 
 
 submitButton.addEventListener('click', getDate);
@@ -169,6 +165,8 @@ function scorpioDetails () {
     pic.src = Scorpio.src;
     caption.textContent = Scorpio.caption;
     box.style.opacity = '1';
+    sound.src = Scorpio.sound;
+    sound.play();
 }
 
 function sagittariusDetails () {
@@ -177,6 +175,8 @@ function sagittariusDetails () {
     pic.src = Sagittarius.src;
     caption.textContent = Sagittarius.caption;
     box.style.opacity = '1';
+    sound.src = Sagittarius.sound;
+    sound.play();
 }
 
 function capricornDetails () {
@@ -185,6 +185,8 @@ function capricornDetails () {
     pic.src = Capricorn.src;
     caption.textContent = Capricorn.caption;
     box.style.opacity = '1';
+    sound.src = Capricorn.sound;
+    sound.play();
 }
 
 function aquariusDetails () {
@@ -193,6 +195,8 @@ function aquariusDetails () {
     pic.src = Aquarius.src;
     caption.textContent = Aquarius.caption;
     box.style.opacity = '1';
+    sound.src = Aquarius.sound;
+    sound.play();
 }
 
 function picesDetails () {
@@ -201,6 +205,8 @@ function picesDetails () {
     pic.src = Pices.src;
     caption.textContent = Pices.caption;
     box.style.opacity = '1';
+    sound.src = Pices.sound;
+    sound.play();
 }
 
 
@@ -229,36 +235,42 @@ const Cancer = {
     value: 'A Cancer\'s personality is like wading chest deep in a lake of warm water. It feels sparkling and cool while you\'re in the shallow end, but you know that if you were to dive in, it would feel warm. The self-awareness of a Cancer is like the tides constantly moving in and out of focus. Their personalities are layered. Cancers have many moods, some of which are contradictory, but they also have a deep, core self that persists. Cancers are weighed down by their own sorrows and the sorrows of those around them. They are frequently haunted by grief. It\'s hard for them to share their pain with others, and they are often afraid to be vulnerable because they carry a fear that people will use their weaknesses against them. Cancers have learned to hide their pain to avoid burdening anyone else. They pretend they\'re okay when they\'re not. Their emotions are like an exposed nerve. Cancers can feel everything. They\'re like a tuning fork that vibrates at the slightest provocation. They tend to carry deep grudges because they can\'t forget the emotional sting of even a slight.',
     src: 'images/bunny.jpeg',
     caption: 'Cancer (06/21 - 07/22)',
+    sound: 'audio/4.mp3',
 }
 
 const Leo = {
     value: ' Leos are bold, warm, and loving. They are also the ultimate performers. They can dazzle with the theatrical flair of a Broadway star and the charisma of a politician. They are captivating personalities. No matter how quickly they\'ve just been introduced to a topic, they can speak eloquently about almost anything because they have such a way with words. Leos inject ambition into everything they do. They are ruled by the heart. Their actions are natural and effortless because they are brash and confident and trust their instincts. Leos have an unrelenting appetite for growth that is easily satiated by the praise of others. When they are hurting, it shows. They will use their anger as a protective wall to shield their vulnerabilities. They want to be perceived as unbreakable because they are scared of admitting vulnerability, needing things from other people, or being incomplete.',
     src: 'images/egg.webp',
     caption: 'Leo (07/23 - 08/22)',
+    sound: 'audio/5.mp3',
 }
 
 const Virgo = {
     value: 'Virgos are known for being perfectionists. They are known for their attention to detail, which helps them find patterns where there are none. At times, Virgos can be finicky and critical to a fault. It\'s true that Virgos are very particular, but that doesn\'t necessarily mean that they keep neat spaces. Their particularities and habits don\'t necessarily line up with traditional views of cleanliness. They could live in what looks like a Tasmanian devil-style dust storm ruin, but still impose a “no shoes in the house” or “no outside clothes on the bed” rule. Maybe their house looks cluttered, but they still know where everything is. Everything has its place. Virgos prefer to exist in organized spaces, but put helping others over their own comfort. A Virgo can become so busy fixing the lives of those around them that they don\'t put much work into providing for their own needs. Virgos are rarely motivated by their own self-interest. Virgos are also known for being intelligent, but because of their introverted nature they can sometimes have trouble expressing themselves. Talking to them may feel like floating on the surface of existence, and like you never know what they\'re thinking and feeling deep inside. Their emphasis on thoughts and ideas can seem like a cover-up for their lack of emotional depth. In reality, they are a fortress unto themselves—the definition of self-containment. Virgos will be endlessly accepting of whatever you have to share, but may not see the value in spontaneously performing a similar excavation of themselves for anyone else.',
     src: 'images/heart.jpeg',
     caption: 'Virgo (08/23 - 09/22)',
+    sound: 'audio/6.mp3',
 }
 
 const Libra = {
     value: 'Libras are difficult to really understand because they seem so contradictory. They\'re simultaneously extroverted and introverted, strategic and spontaneous, focused and intuitive. This variability makes it difficult to pin down their true character. They are an entire constellation of personalities. Libras are different depending on who they\'re around. This is because they value empathy and want to be receptive. They can be other people\'s mirrors. While they have strong opinions about other people, it can take a long time for them to understand themselves. Libras want to be admired not for their talents but for their charm. They want to be seen as the responsible figure who can balance out the turbulent elements around them. Libras feel an intense amount of guilt and shame when they are unhappy because they know that their unhappiness affects everyone around them.',
     src: 'images/bear.jpeg',
     caption: 'Libra (09/23 - 10/22)',
+    sound: 'audio/7.mp3',
 }
 
 const Scorpio = {
     value: 'A Scorpio\'s personality is a chasm of infinite complexity (or at least how they project themselves). They are difficult people to get to know because they are psychological trap doors. Scorpios socialize from behind a double-sided mirror, always scanning, reading you while you can only see your own reflection. They prefer to be the people asking the questions, removing your skin with their perceptive scalpel and taking inventory of your pulsing viscera. They subtly probe and push your pressure points so they can get the answer they\'re seeking. Scorpios are keenly aware of power, its flows, and their position within its matrix. Despite their ability to be popular, there\'s something very lonely about Scorpios. They have a ruthless view of the world. Eat or be eaten. Their knowledge of other people\'s internal structures gives them an edge over the competition. They know how to play people against each other. Every human interaction is a meeting of opposing powerful forces. A clashing of wants and needs where somebody wins and somebody loses. They are forthcoming and evasive at the same time, depending on what is situationally beneficial. They are blunt, but not without tact. They are intentional about the information they reveal. They\'re not liars, they don\'t spread lies, but they are masters of opening windows on certain truths while concealing others behind heavy curtains.',
     src: 'images/cat.webp',
     caption: 'Scorpio (10/23 - 11/21)',
+    sound: 'audio/8.mp3',
 }
 
 const Sagittarius = {
     value: 'Sagittarians are the ultimate empiricists. They will always choose principles over feelings and will often question who they are. They move from job to job, philosophy to philosophy, belief to belief. They are explorers of the human condition and are unafraid of change. Sagittarians feel like the world is their playground. They love to explore the unknown. At their core, they want to understand how the world works. On an unbounded quest to discover the whole of the universe inside of themselves, Sagittarians are unshackled from any particular worldly attachment. They are reckless in their pursuit of what they want, and often end up doing and experiencing things that defy conceptions of the possible. Sagittarians are explorers. They are both the fearless adventurers and the jaded critics. They understand that knowledge comes in two forms: the shallow, disposable kind that comes from external sources, and the kind that comes from within. Sagittarians know that external knowledge can be easily gained while internal knowledge is exponentially deeper and more powerful.',
     src: 'images/dino.jpeg',
     caption: 'Sagittarius (11/22 - 12/21)',
+    sound: 'audio/9.mp3',
 }
 
 
@@ -266,18 +278,21 @@ const Capricorn = {
     value: "Capricorns aren\'t consumed by the need for attention, but are not immune to it. They simply don\'t indulge in attention-seeking behavior. Capricorns don\'t want attention for their looks or style or ingenuity. They don\'t want to be admired, but they do want to be respected. They don\'t care to be the center of attention. Instead it\'s like they need to prove their own abilities to themselves. They plow through adversity with blinders on until they reach success. Capricorns are constantly competing with themselves. They do want praise, though. Praise is a simple acknowledgment of a job well done. Praise is a pat on the back. They use this as validation to compensate for general feelings of incompetence and inferiority.",
     src: 'images/dog.jpeg',
     caption: 'Capricorn (12/22 - 01/19)',
+    sound: 'audio/10.mp3',
 }
 
 const Aquarius = {
     value: "Aquarians are archetypal outcasts. This doesn\'t mean they\'re loners. In fact, they thrive in large groups—charming you with their peculiar senses of humor, intriguing you with fun facts about the history of disposable straws, or convincing you to join their reading groups. The alienation they feel is often self-imposed—a result of their knee-jerk contrarianism, rather than a lack of social intelligence. They try to be weird. Aquarians hang grapefruit rinds from the wall and call it art, they pretend to actually like noise music, they saturate their internal monologues with SAT words. Intelligence, to them, means the ability to formulate the most unpopular opinion possible. If they were white sheep, they\'d dye their wool black just to prove a point. They are destined to live their lives in direct opposition to the current world. In fact, they are world-builders. An entire universe exists in their heads, and this universe adheres to its own set of logic that doesn\'t map onto reality. They aren't anarchists. They are utopians. They are not cold. They are rational. To them, emotions are just holes in their idealistic vessels. Why succumb to sensitivity when there is so much knowledge to acquire, so many problems to be solved, worlds to invent?They sometimes ask, “Why can\'t I be normal?” but in reality, they find normalcy boring. They want to be unknowable. Aquarians want to evade definition. Definitions are binding little rules that other people use to restrict their movement, and they must resist anything that infringes on their freedom to drift. Between definitions, between the individual and the common, between themselves and humanity. The ultimate Aquarian struggle is resolving the tension between their need for community, and their need for complete detachment. Aquarians are always running little tests. They live their lives as one big science experiment, using the element of surprise to collect reactions and construct theories about the intricacies of human nature. They are walking abstract art pieces, shattering traditional perspectives with their complete disregard for convention. They are martyrs to the cause of humanity. Freedom is their only demand.",
     src: 'images/duck.webp',
     caption: 'Aquarius (01/20 - 02/18)',
+    sound: 'audio/11.mp3',
 }
 
 const Pices = {
     value: "Describing a Pisces\' personality can be difficult because Pisces tend to evade distinction. Their behavior changes significantly based on who they\'re around. Pisces are just permeable membranes that pensively let things flow through them. They are cerebral sea sponges. They are boundless, diluting themselves with larger personalities to avoid having to form coherent identities. Most of the qualities usually associated with Pisces (dreaminess, emotionality, imagination) are internal processes that are difficult to observe from the outside. Pisces are primarily inward-facing. They are not self-absorbed, but they are absorbed in themselves. Ultimately, a Pisces wants to dissolve. To wrest themselves of a physical form and diffuse into everything they touch as love. To exist in the gossamer liminal layer that hangs just barely above the material. To live their life like a romantic poem. To understand that reality and fantasy are neighbors on the same infinite plane.",
     src: 'images/octopus.jpeg',
     caption: 'Pices (02/19 - 03/20)',
+    sound: 'audio/12.mp3',
 }
 
 
